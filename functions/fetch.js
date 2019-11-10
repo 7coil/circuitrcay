846f5d10cf7d8756a2c3275755349f8ea64af2c2
Katie <katie@discordmail.com>
2019-11-10 00:00:11 +0000

    exports smh

--- functions/fetch.js
+++ functions/fetch.js
@@ -1,6 +1,6 @@
 const fetch = require('node-fetch');
 
-event.handler = (event, context, callback) => {
+exports.handler = (event, context, callback) => {
   // https://github.com/DavidWells/netlify-functions-workshop/blob/master/lessons-code-complete/core-concepts/2-dynamic-content/functions/using-path.js
   const path = event.path.replace(/\/\.netlify\/functions\/[^/]*/, '');
   const url = new URL(`https://phoneadmin.flashcashonline.com${path}`);
