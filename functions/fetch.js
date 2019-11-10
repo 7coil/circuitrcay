ab3b610479c2bbe4c23b9e03e812e8f8427e6c09
Katie <katie@discordmail.com>
2019-11-10 00:03:31 +0000

    use node url

--- functions/fetch.js
+++ functions/fetch.js
@@ -1,14 +1,15 @@
 const fetch = require('node-fetch');
+const url = require('url');
 
 exports.handler = (event, context, callback) => {
   // https://github.com/DavidWells/netlify-functions-workshop/blob/master/lessons-code-complete/core-concepts/2-dynamic-content/functions/using-path.js
   const path = event.path.replace(/\/\.netlify\/functions\/[^/]*/, '');
-  const url = new URL(`https://phoneadmin.flashcashonline.com${path}`);
+  const myURL = url.parse(`https://phoneadmin.flashcashonline.com${path}`);
 
   Object.keys(event.queryStringParameters)
-    .forEach(key => url.searchParams.append(key, params[key]));
+    .forEach(key => myURL.searchParams.append(key, params[key]));
 
-  fetch(url, {
+  fetch(myURL, {
     headers: event.headers,
     method: event.httpMethod
   })
