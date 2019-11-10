370aa0a9345ca55f294731d3b63e103ec691a6c2
Katie <katie@discordmail.com>
2019-11-10 00:08:39 +0000

    dont use legacy url

--- functions/fetch.js
+++ functions/fetch.js
@@ -1,13 +1,14 @@
 const fetch = require('node-fetch');
-const url = require('url');
 
 exports.handler = (event, context, callback) => {
   // https://github.com/DavidWells/netlify-functions-workshop/blob/master/lessons-code-complete/core-concepts/2-dynamic-content/functions/using-path.js
   const path = event.path.replace(/\/\.netlify\/functions\/[^/]*/, '');
-  const myURL = url.parse(`https://phoneadmin.flashcashonline.com${path}`);
-
-  Object.keys(event.queryStringParameters)
-    .forEach(key => myURL.searchParams.append(key, params[key]));
+  
+  const qs = Object.keys(event.queryStringParameters)
+    .map(key => `${key}=${encodeURIComponent(event.queryStringParameters[key])}`)
+    .join('&')
+  
+  const myURL = `https://phoneadmin.flashcashonline.com${path}${qs.length && `?${qs}`}`
 
   fetch(myURL, {
     headers: event.headers,
